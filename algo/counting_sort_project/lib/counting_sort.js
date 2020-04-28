2a7a5c0baa48ab6e35b9d919d02a7818246dd4e5
mmouss2 <moussa.majid@hotmail.com>
2020-04-27 22:56:56 -0400

    class Inheritance

--- algo/counting_sort_project/lib/counting_sort.js
+++ algo/counting_sort_project/lib/counting_sort.js
@@ -1,3 +1,9 @@
+
+
+//this pair with Sadman:
+
+
+
 // function countingSort(arr, max) {
 //   const result = [];
 
@@ -37,4 +43,51 @@ function countingSort(arr, max){
 
 module.exports = {
     countingSort
+};
+
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currentEl = arr[i];
+        for (var j = i - 1; j >= 0 && currentEl < arr[j]; j--) {
+            arr[j + 1] = arr[j];
+            // arr[j] = currentEl;
+        }
+        arr[j + 1] = currentEl;
+
+    }
+    return arr;
+}
+
+module.exports = {
+    insertionSort
+};
+
+let arr = [3, 2, 5, 1]
+insertionSort(arr);
+console.log(arr);
+
+
+
+ ///................................................................
+
+/// these pair with David:
+
+
+
+// Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
+// Feel free to use this file for scratch work.
+
+var change = function (amount, coins) {
+    let arr = new Array[n];  // [1]
+
+    let sum = 0                       // [1,2,5]
+    for (let i = 1; i <= amount; i++) {   //1,2,3,4,5
+        if (i in coins) {
+            //sum += i  //sum = 3   [1]
+            arr.push(i)
+            let result = change(amount, arr[i - 1])
+        }
+    }
+
 };
